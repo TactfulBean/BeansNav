d4ff210727436fbf1b081415162c39398e528433
TactfulBean <tactfulbean@gmail.com>
2023-05-02 11:46:59 +0800

    update

---
+++ src/components/LinkBox.vue
@@ -0,0 +1,102 @@
+<template>
+	<div id="carousel">
+		<a-carousel>
+			<div class="layout-col layout-row">
+				<!--app-group-item-->
+				<div class="app-group-item">
+					<div>
+						<a-button shape="circle"><icon-font type="icon-bing" style="color: #008b8b" /></a-button>
+					</div>
+					<span>111</span>
+				</div>
+				<div class="app-group-item">
+					<div>
+						<a-button shape="circle"><icon-font type="icon-bing" style="color: #008b8b" /></a-button>
+					</div>
+					<span>111</span>
+				</div>
+				<div class="app-group-item">
+					<div>
+						<a-button shape="circle"><icon-font type="icon-bing" style="color: #008b8b" /></a-button>
+					</div>
+					<span>111</span>
+				</div>
+			</div>
+			<div class="layout-col layout-row">
+				<!--app-group-item-->
+				<div class="app-group-item">
+					<div>
+						<a-button shape="circle"><icon-font type="icon-bing" style="color: #008b8b" /></a-button>
+					</div>
+					<span>222</span>
+				</div>
+				<div class="app-group-item">
+					<div>
+						<a-button shape="circle"><icon-font type="icon-bing" style="color: #008b8b" /></a-button>
+					</div>
+					<span>222</span>
+				</div>
+			</div>
+		</a-carousel>
+	</div>
+</template>
+
+<script setup lang="ts">
+import { createFromIconfontCN } from "@ant-design/icons-vue";
+
+const IconFont = createFromIconfontCN({
+	scriptUrl: "//at.alicdn.com/t/c/font_3627162_97fzu7jybss.js"
+});
+</script>
+
+<style scoped lang="less">
+#carousel {
+	position: absolute;
+	width: 70%;
+	height: 320px;
+	left: 50%;
+	top: 400px;
+	transform: translateX(-50%);
+}
+
+.ant-carousel :deep(.slick-slide) {
+	text-align: center;
+	height: 320px;
+}
+.app-group-item {
+	display: flex;
+	justify-content: center;
+	flex-direction: column;
+	padding: 15px;
+	.ant-btn-circle {
+		min-width: 64px;
+		min-height: 64px;
+	}
+}
+.layout-col {
+	.app-group-item {
+		margin-top: 20px;
+		float: left;
+	}
+}
+.layout-row {
+	.app-group-item {
+		width: calc(100% / 8);
+	}
+}
+//max-width<400px
+@media screen and (max-width: 400px) {
+}
+//max-width<600px
+@media screen and (max-width: 600px) {
+	#carousel {
+		top: 323px;
+		width: 80%;
+	}
+	.layout-row {
+		.app-group-item {
+			width: calc(100% / 3);
+		}
+	}
+}
+</style>
---
+++ src/components/Time.vue
@@ -0,0 +1,56 @@
+<template>
+	<div id="time-container">
+		<div id="time-text">{{ time }}</div>
+	</div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+const time = ref<string>("");
+const updateTime = () => {
+	const date = new Date();
+	const hours = date.getHours().toString().padStart(2, "0");
+	const minutes = date.getMinutes().toString().padStart(2, "0");
+	time.value = `${hours}:${minutes}`;
+};
+
+updateTime();
+setInterval(updateTime, 1000);
+</script>
+
+<style scoped lang="less">
+//时间栏
+#time-container {
+	position: fixed;
+	top: 115px;
+	left: 50%;
+	transform: translateX(-50%);
+	padding: 10px;
+	text-align: center;
+	transition: 0.3s;
+}
+#time-text {
+	max-width: 300px;
+	color: #fff;
+	font-family: -apple-system, BlinkMacSystemFont;
+	font-weight: 400;
+	font-size: 42px;
+	white-space: nowrap;
+	text-overflow: ellipsis;
+	transition: 0.3s;
+}
+#time-text:hover {
+	scale: 1.2;
+	transition: 0.3s;
+}
+//max-width<400px
+@media screen and (max-width: 400px) {
+}
+//max-width<600px
+@media screen and (max-width: 600px) {
+	#time-container {
+		top: 38px;
+	}
+}
+</style>
