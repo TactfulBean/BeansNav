eb15ccfaefe3f4ca89b0f35f2143bfb6caa3e388
TactfulBean <2662367665@qq.com>
2023-07-28 16:55:47 +0800

    pref:搜索建议请求优化

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,4 +1,5 @@
 import axios from "@/utils/request"
+import jsonpAdapter from "axios-jsonp"
 
 const WEATHER_URL = import.meta.env.VITE_WEATHER
 const LINKBOX_URL = import.meta.env.VITE_LINKBOX
@@ -20,4 +21,19 @@ export const getUpdateLog = () => {
 		method: "GET",
 		url: UPDATELOG_URL
 	})
+}
+export const getSearchSuggestions = async (keyWord) => {
+	try {
+		// @ts-ignore
+		const response = await axios({
+			url: `https://suggestion.baidu.com/su?wd=${keyWord}&cb=json`,
+			adapter: jsonpAdapter,
+			callbackParamName: "cb"
+		})
+		// @ts-ignore
+		return response.s
+	} catch (error) {
+		console.error("处理搜索建议发生错误：", error)
+		return null
+	}
 }
