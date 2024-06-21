dfb6f7a8e39490e039cfa38552fed3c24a82a658
TactfulBean <2662367665@qq.com>
2024-06-21 15:55:35 +0800

    feat:LinkBox测试样式

--- src/views/Main/components/LinkBox.vue
+++ src/views/Main/components/LinkBox.vue
@@ -2,27 +2,27 @@
   <transition name="fade">
     <div class="bg-color_bg_opacity rounded-2xl w-full flex-1 flex-col gap-10 px-10 py-10">
       <div class="h-30 w-full flex gap-10">
-        <div class="w-auto rounded-2xl px-10 border flex-center" @click="page = 1">1</div>
-        <div class="w-auto rounded-2xl px-10 border flex-center" @click="page = 2">2</div>
-        <div class="w-auto rounded-2xl px-10 border flex-center" @click="page = 3">3</div>
+        <div v-for="(item, index) in LinkList" class="w-auto rounded-2xl px-10 border flex-center" @click="page = index + 1">{{ item.name }}</div>
       </div>
-      <div class="flex-1 overflow-hidden">
-        <transition :duration="0" name="fade">
-          <div v-show="page == 1" class="rounded-2xl wh-full bg-color_bg_opacity">111</div>
-        </transition>
-        <transition :duration="0" name="fade">
-          <div v-show="page == 2" class="rounded-2xl wh-full bg-color_bg_opacity">2222</div>
-        </transition>
-        <transition :duration="0" name="fade">
-          <div v-show="page == 3" class="rounded-2xl wh-full bg-color_bg_opacity">3333333</div>
+      <div class="flex-1 overflow-hidden relative">
+        <transition v-for="(item, index) in LinkList" name="fade">
+          <div v-show="page == index + 1" class="rounded-2xl wh-full absolute bg-color_bg_opacity">{{ item.name }}</div>
         </transition>
       </div>
     </div>
   </transition>
 </template>
 <script lang="ts" setup>
-import { ref } from "vue"
-
+import { onMounted, ref } from "vue"
+import Request from "@/utils/request.ts"
 const page = ref(1)
+
+const LinkList = ref()
+
+onMounted(() => {
+  Request.get("./config/LinkList.json").then((res) => {
+    LinkList.value = res.data
+  })
+})
 </script>
 <style lang="scss" scoped></style>
