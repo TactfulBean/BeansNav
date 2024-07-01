469bbdac45d47838ebe22b8694d1c492e1c8b004
TactfulBean <2662367665@qq.com>
2024-07-01 11:28:50 +0800

    fix:部署错误修复

--- src/components/ThemeSwitch.vue
+++ src/components/ThemeSwitch.vue
@@ -16,35 +16,35 @@ onMounted(() => {
   mainStore.darkTheme ? document.body.setAttribute("arco-theme", "dark") : document.body.removeAttribute("arco-theme")
 })
 
-const change = (event: any) => {
-  mainStore.darkTheme = !mainStore.darkTheme
-  mainStore.darkTheme ? document.body.setAttribute("arco-theme", "dark") : document.body.removeAttribute("arco-theme")
-
-  const x = event.clientX
-  const y = event.clientY
-  const endRadius = Math.hypot(Math.max(x, innerWidth - x), Math.max(y, innerHeight - y))
-  let isDark: boolean
-
-  // @ts-ignore
-  const transition = document.startViewTransition(() => {
-    const root = document.documentElement
-    isDark = root.classList.contains("dark")
-    root.classList.remove(isDark ? "dark" : "light")
-    root.classList.add(isDark ? "light" : "dark")
-  })
-  transition.ready.then(() => {
-    const clipPath = [`circle(0px at ${x}px ${y}px)`, `circle(${endRadius}px at ${x}px ${y}px)`]
-    document.documentElement.animate(
-      {
-        clipPath: isDark ? clipPath.reverse() : clipPath
-      },
-      {
-        duration: 500,
-        easing: "ease-in",
-        pseudoElement: isDark ? "::view-transition-old(root)" : "::view-transition-new(root)"
-      }
-    )
-  })
-}
+// const change = (event: any) => {
+//   mainStore.darkTheme = !mainStore.darkTheme
+//   mainStore.darkTheme ? document.body.setAttribute("arco-theme", "dark") : document.body.removeAttribute("arco-theme")
+//
+//   const x = event.clientX
+//   const y = event.clientY
+//   const endRadius = Math.hypot(Math.max(x, innerWidth - x), Math.max(y, innerHeight - y))
+//   let isDark: boolean
+//
+//   // @ts-ignore
+//   const transition = document.startViewTransition(() => {
+//     const root = document.documentElement
+//     isDark = root.classList.contains("dark")
+//     root.classList.remove(isDark ? "dark" : "light")
+//     root.classList.add(isDark ? "light" : "dark")
+//   })
+//   transition.ready.then(() => {
+//     const clipPath = [`circle(0px at ${x}px ${y}px)`, `circle(${endRadius}px at ${x}px ${y}px)`]
+//     document.documentElement.animate(
+//       {
+//         clipPath: isDark ? clipPath.reverse() : clipPath
+//       },
+//       {
+//         duration: 500,
+//         easing: "ease-in",
+//         pseudoElement: isDark ? "::view-transition-old(root)" : "::view-transition-new(root)"
+//       }
+//     )
+//   })
+// }
 </script>
 <style lang="scss" scoped></style>
