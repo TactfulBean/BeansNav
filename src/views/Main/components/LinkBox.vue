7a3b9e2b03b59c53400ce33968ef08128bcbd57f
TactfulBean <2662367665@qq.com>
2024-07-02 10:28:39 +0800

    fix: 修复左右箭头初始化不显示的问题

--- src/views/Main/components/LinkBox.vue
+++ src/views/Main/components/LinkBox.vue
@@ -1,14 +1,11 @@
 <template>
   <transition name="fade">
-    <div class="box duration-300 rounded-2xl w-full flex-1 flex-col gap-10 px-10 py-10">
+    <div v-if="LinkList.length > 0" class="box duration-300 rounded-2xl w-full flex-1 flex-col gap-10 px-10 py-10">
       <div class="flex items-center">
         <transition name="fade">
           <div v-if="!arrivedState.left" class="leftArrow absolute left-0 text-20 w-20 text-[#dddddd] i-carbon:chevron-left"></div>
         </transition>
-        <div
-          ref="scrollRef"
-          class="scrollBar flex-1 duration-300 h-30 w-full flex gap-10 whitespace-nowrap overflow-x-scroll overflow-y-hidden shadow-inner"
-        >
+        <div ref="scrollRef" class="scrollBar duration-300 h-30 flex gap-10 whitespace-nowrap overflow-x-scroll overflow-y-hidden shadow-inner">
           <div v-for="(item, index) in LinkList" class="w-auto rounded-8 px-10 border flex-center text-[#dddddd]" @click="page = index + 1">
             {{ item.name }}
           </div>
@@ -31,7 +28,7 @@ import { onMounted, ref } from "vue"
 import Request from "@/utils/request.ts"
 const page = ref(1)
 
-const LinkList = ref()
+const LinkList = ref<any>([])
 
 const scrollRef = ref()
 const { arrivedState } = useScroll(scrollRef)
